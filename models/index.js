c0a2f8b00760aa4027341809b0dc9cfe41d6fd80
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-20 13:38:39 -0400

    made final changes to the models folder

--- models/index.js
+++ models/index.js
@@ -6,13 +6,14 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'id',
+  unique: true
 
 })
 
 // Categories have many Products.
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
+  foreignKey: 'id',
   onDelete: 'CASCADE'
 })
 
