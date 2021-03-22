f88dbdd326c61336dc6106ab452a739d31d57004
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-22 19:47:18 -0400

    added async to all of product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/',  async (req, res) => {
   try {
     const productData = await Product.findAll({
       include: [{ model: Category }],
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id',  async (req, res) => {
   try {
     const productData = await Category.findByPk({
       include: [{ model: Category }],
@@ -111,7 +111,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const productData = await Product.destroy({
       where: { id: req.params.id },
