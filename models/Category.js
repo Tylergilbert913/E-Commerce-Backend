53fec4f718d4c84a3a0e0ace95a72fd038f6d3cf
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-23 23:06:11 -0400

    commented out the models

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+// tables created with models
 Category.init(
   {
     id: {
--- models/Tag.js
+++ models/Tag.js
@@ -3,7 +3,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
-
+// tables created with models
 Tag.init(
   {
     // defined columns
